7fcb43fb05df5702acdc0301dbf82304ab5a4dca
OCHyams <Orlando2.Cazalet-Hyams@live.uwe.ac.uk>
2016-09-28 19:10:09 +0100

    (Re)Added Ball.cs and BallSpawner.cs

---
+++ Astro Conk'em/Assets/Scripts/Ball.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    public  Rigidbody m_rigidBody;  //public to provide an easy-to-reach reference, please don't go reassigning this! This is a jam, jam is messy.
+    [SerializeField]
+    private bool m_isActive = false;    
+    private MeshRenderer m_renderer;
+    private SphereCollider m_collider;
+    private float m_timer =0.0f;
+    [SerializeField]
+    private float m_despawnTime = 5.0f; //low for now for testing
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Cache component references
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_renderer = GetComponent<MeshRenderer>();
+        m_collider = GetComponent<SphereCollider>();
+
+        //Set component initial state 
+        m_rigidBody.isKinematic = true;
+        m_renderer.enabled = false;
+        m_collider.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (m_isActive)
+        {
+            m_timer += Time.deltaTime;
+            if (m_timer >= m_despawnTime)
+            {
+                m_timer = 0;
+                setActive(false);
+            }
+        }
+	}
+
+    public bool isActive() { return m_isActive; }
+
+    public void setActive(bool _active)
+    {
+        //If there has been a change...
+        if (m_isActive != _active)
+        {
+            m_rigidBody.isKinematic = !m_rigidBody.isKinematic;
+            m_renderer.enabled = !m_renderer.enabled;
+            m_collider.enabled = !m_collider.enabled;
+        }
+
+        m_isActive = _active;
+    }
+}
---
+++ Astro Conk'em/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour
+{
+   
+    public GameObject m_ballPrefab;
+
+    [SerializeField]
+    private Vector3 m_baseSpawnForce = new Vector3( 0,200, 0);
+    [SerializeField]
+    private const int m_maxBalls = 20;
+    private Ball[] m_balls;
+
+	void Start ()
+    {
+        //Init ball array
+        m_balls = new Ball[m_maxBalls];
+
+        //Instantiate new balls from the m_ballPrefab prefab, cache reference to Ball script
+        for (int i = 0; i < m_maxBalls; ++i)
+        {
+            m_balls[i] = Instantiate(m_ballPrefab).GetComponent<Ball>();
+        }
+
+
+    }
+
+	void Update ()
+    {
+        //Just for testing; activate the spawner with spacebar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spawnBall();
+        }
+	}
+
+    //spawn a ball, returns false if it wasn't possible (too many balls in play)
+    public bool spawnBall()
+    {
+        //Find a free ball...
+        for (int i = 0; i < m_maxBalls; ++i)
+        {
+            if (m_balls[i].isActive() == false)
+            {
+                m_balls[i].setActive(true);
+                m_balls[i].gameObject.transform.position = gameObject.transform.position;
+                m_balls[i].m_rigidBody.AddForce(m_baseSpawnForce);
+                return true;//early out
+            }
+        }
+
+        return false;
+    }
+  
+}
