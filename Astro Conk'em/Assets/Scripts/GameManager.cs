158ca5369adc67f2a0b8dac8f6e59ff00a6cc198
thomas465 <thomas465@live.co.uk>
2016-09-29 14:53:26 +0100

    More work on title

--- Astro Conk'em/Assets/Scripts/GameManager.cs
+++ Astro Conk'em/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     public enum STATE
     {
         start,
+        title,
         game,
         gameover
     };
---
+++ Astro Conk'em/Assets/Scripts/TitleScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+
+    public Text astro, conkem, prompt;
+
+	// Use this for initialization
+	void Start () {
+        GameManager.g_GameManager.registerState((int)GameManager.STATE.title, StartTitle, EndTitle);
+	}
+
+    void StartTitle()
+    {
+
+    }
+
+    void EndTitle()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        conkem.transform.localScale = Vector3.one * (1 + (Mathf.Sin(Time.timeSinceLevelLoad*3))*0.1f);
+
+        prompt.transform.Rotate(Vector3.up * Time.deltaTime);
+	}
+}
