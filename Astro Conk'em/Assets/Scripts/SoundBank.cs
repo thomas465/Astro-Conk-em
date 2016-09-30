9fa40a0194dcce5ad20b384be9760e2e7b5411d7
t2-millssmith <Thomas465@live.co.uk>
2016-09-30 18:45:20 +0100

    Started work on sounds

---
+++ Astro Conk'em/Assets/Scripts/SoundBank.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundBank : MonoBehaviour {
+
+    public static SoundBank sndBnk;
+
+    public AudioClip menuClick;
+
+    public AudioClip batHitBall;
+    public AudioClip[] ballHits;
+
+    public AudioClip[] slugVoices;
+
+    public AudioClip[] playerSwingVoices, playerHurtVoices, playerDeathSounds;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
