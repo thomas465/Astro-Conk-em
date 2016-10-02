4f15164d49b51b8ad6d6262dd727f1d6b7d28896
thomas465 <thomas465@live.co.uk>
2016-10-02 14:26:41 +0100

    Juiced up the HUD

--- Astro Conk'em/Assets/Scripts/PlayerHealth.cs
+++ Astro Conk'em/Assets/Scripts/PlayerHealth.cs
@@ -6,17 +6,20 @@ public class PlayerHealth : MonoBehaviour {
 
     public static PlayerHealth healthSingleton;
 
-    public Slider m_slider;
 	public Image m_hurtFlash;
     
 	public int m_startingPlayerHealth = 100;
 	public int m_currentPlayerHealth;
 	private bool m_playerDamaged = false;
 
+    public Image fill, hurtFill;
+    public Transform healthBarParent;
+
 	void Start () 
 	{
         healthSingleton = this;
         m_currentPlayerHealth = m_startingPlayerHealth;
+        fill.fillAmount = 1;
         //reference to a player movement script
 	}
 
@@ -30,17 +33,23 @@ public class PlayerHealth : MonoBehaviour {
 		//else 
 		//{
 			m_hurtFlash.color = Color.Lerp (m_hurtFlash.color, Color.clear, 5 * Time.deltaTime);
-		//}
-			
+        //}
+
+        hurtFill.fillAmount = Mathf.Lerp(hurtFill.fillAmount, fill.fillAmount, 2 * Time.deltaTime);
+
+        healthBarParent.transform.localScale = Vector3.Lerp(healthBarParent.transform.localScale, Vector3.one * 0.7617157f, 10 * Time.deltaTime);
 	}
 
     public void TakeDamage(int amount)
 	{
-        m_hurtFlash.color = new Color(1f, 0f, 0f, 0.9f);
+        healthBarParent.transform.localScale = Vector3.one * 1.25f;
+        m_hurtFlash.color = new Color(1f, 0f, 0f, 0.75f);
         //to be called in another script
         m_startingPlayerHealth -= amount;
 		m_playerDamaged = true;
-		m_slider.value = m_startingPlayerHealth;
+
+
+        fill.fillAmount = (float)m_startingPlayerHealth / 100;
 
 		if (m_startingPlayerHealth <= 0)
 		{
