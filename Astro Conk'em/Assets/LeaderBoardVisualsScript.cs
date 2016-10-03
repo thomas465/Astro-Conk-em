a2f968462e4719408b6ebe721c3eb3f50af3abd4
thomas465 <thomas465@live.co.uk>
2016-10-03 13:37:36 +0100

    Started work on leaderboard visuals

---
+++ Astro Conk'em/Assets/LeaderBoardVisualsScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeaderBoardVisualsScript : MonoBehaviour
+{
+
+    public static LeaderBoardVisualsScript singleton;
+    bool active = false;
+
+    public GameObject entryPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        singleton = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Activate()
+    {
+        active = true;
+        Vector3 entryPos = entryPrefab.transform.localPosition;
+
+        for(int i=0; i<5; i++)
+        {
+            Instantiate(entryPrefab, entryPos, Quaternion.LookRotation(transform.forward));
+            entryPos -= Vector3.up * 85;
+        }
+    }
+}
